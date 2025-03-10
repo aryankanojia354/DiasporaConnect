c57a98493b4b0468d394ed6d2ee55a6960c67aa1
Aryan Kanojia <aryankanojia354@gmail.com>
2025-03-10 20:26:52 +0000

    express module installed

--- server/controllers/shop/order-controller.js
+++ server/controllers/shop/order-controller.js
@@ -2,6 +2,7 @@ const paypal = require("../../helpers/paypal");
 const Order = require("../../models/Order");
 const Cart = require("../../models/Cart");
 const Product = require("../../models/Product");
+const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
 const createOrder = async (req, res) => {
   try {
