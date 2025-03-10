906eceda4473e5203125a9f71ea0370550df97ae
Aryan Kanojia <aryankanojia354@gmail.com>
2025-03-10 14:18:41 +0000

    payment method fixed

--- server/controllers/shop/order-controller.js
+++ server/controllers/shop/order-controller.js
@@ -2,6 +2,7 @@ const paypal = require("../../helpers/paypal");
 const Order = require("../../models/Order");
 const Cart = require("../../models/Cart");
 const Product = require("../../models/Product");
+const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
 const createOrder = async (req, res) => {
   try {
@@ -26,8 +27,8 @@ const createOrder = async (req, res) => {
         payment_method: "paypal",
       },
       redirect_urls: {
-        return_url: "http://localhost:5173/shop/paypal-return",
-        cancel_url: "http://localhost:5173/shop/paypal-cancel",
+        return_url: `${backendUrl}/shop/paypal-return`,
+        cancel_url: `${backendUrl}/shop/paypal-cancel`,
       },
       transactions: [
         {
