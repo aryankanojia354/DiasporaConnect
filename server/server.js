779142d426caa55f6ee5f5a6a48429bc88f28d75
Aryan Kanojia <aryankanojia354@gmail.com>
2025-03-10 09:57:33 +0000

    domain changed

--- server/server.js
+++ server/server.js
@@ -37,7 +37,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://diaspora-connect-gkep.vercel.app/",
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
