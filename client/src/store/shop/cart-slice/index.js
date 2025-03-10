d6118f1000998d0b0978f2035408bf69c99bdf68
Aryan Kanojia <aryankanojia354@gmail.com>
2025-03-10 12:39:41 +0000

    done .env

--- client/src/store/shop/cart-slice/index.js
+++ client/src/store/shop/cart-slice/index.js
@@ -27,7 +27,7 @@ export const fetchCartItems = createAsyncThunk(
   "cart/fetchCartItems",
   async (userId) => {
     const response = await axios.get(
-      `http://localhost:5000/api/shop/cart/get/${userId}`
+      `${backendUrl}/api/shop/cart/get/${userId}`
     );
 
     return response.data;
@@ -38,7 +38,7 @@ export const deleteCartItem = createAsyncThunk(
   "cart/deleteCartItem",
   async ({ userId, productId }) => {
     const response = await axios.delete(
-      `http://localhost:5000/api/shop/cart/${userId}/${productId}`
+      `${backendUrl}/api/shop/cart/${userId}/${productId}`
     );
 
     return response.data;
@@ -49,7 +49,7 @@ export const updateCartQuantity = createAsyncThunk(
   "cart/updateCartQuantity",
   async ({ userId, productId, quantity }) => {
     const response = await axios.put(
-      "http://localhost:5000/api/shop/cart/update-cart",
+      `${backendUrl}/api/shop/cart/update-cart`,
       {
         userId,
         productId,
