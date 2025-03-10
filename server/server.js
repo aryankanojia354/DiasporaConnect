5b656adbe10f1a25455f20342bfadee45ba51b7a
Aryan Kanojia <aryankanojia354@gmail.com>
2025-03-10 11:44:35 +0000

    done .env

--- server/server.js
+++ server/server.js
@@ -37,7 +37,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "https://diaspora-connect-gkep.vercel.app/",
+    origin: "https://diaspora-connect-gkep.vercel.app",
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
